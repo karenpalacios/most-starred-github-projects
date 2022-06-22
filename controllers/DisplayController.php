8e2734f22d78dccb57c3bbe439e5bb82f13edb26
Karen Palacios <karen.palacios@tambourine.com>
2022-06-22 16:52:02 -0400

    Get data from database

--- controllers/DisplayController.php
+++ controllers/DisplayController.php
@@ -4,7 +4,11 @@ include_once('controllers/BaseController.php');
 class DisplayController extends BaseController{
 
     function index() {
-     
+
+        $repositories = new Repositories();
+        $repositories->getAll(); 
+        
+        
         $this->render('main');
     }
 
