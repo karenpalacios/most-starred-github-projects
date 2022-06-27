bc890a42949ea54994297dc5ffedc0394111c8a1
Karen Palacios <karen.palacios@tambourine.com>
2022-06-27 12:07:37 -0400

    Add insert method

--- core/Database.php
+++ core/Database.php
@@ -3,9 +3,27 @@
 class Database {
 
     public function __construct() {
-		return $this->connection = new Mysql(Config::Database('mysql','host'),
+	  	return $this->connection = new Mysql(Config::Database('mysql','host'),
                                       Config::Database('mysql','user'),
                                       Config::Database('mysql','pass'),
                                       Config::Database('mysql','db'));
     }
+
+    function insert($table,$data) {        
+      $fields = array_keys($data);
+      $values = array_values($data);
+      try {
+          $query  = 'INSERT INTO '. $table . '(';
+          $query .= implode(',',$fields);
+          $query .= ') ';
+          $query .= 'VALUES (';
+          $query .= implode(',', array_fill(0, count($fields), '?'));
+          $query .= ')';
+          array_unshift($values , $query);
+          $result = $this->connection->query(...$values);
+      } catch (Exception $e) {
+          return 'Database exception: ' . $e->getMessage() . PHP_EOL;
+      }
+      return $result;
+  }  
 }
